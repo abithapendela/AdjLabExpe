58e93812cac5f2275a4300e54af095d1a2baad3f
abithapendela <abithapendela.com>
2021-02-05 13:44:14 +0530

    HTML response redirection

---
+++ Exp6/src/Exp6.java
@@ -0,0 +1,65 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Fourth
+ */
+@WebServlet("/Exp6")
+public class Exp6 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Exp6() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String value = request.getParameter("websites");
+
+		if(value.equals("google")) {
+			response.sendRedirect("https://www.google.com");
+		}
+		else if(value.equals("facebook")) {
+			response.sendRedirect("https://www.facebook.com");
+		}
+		else if(value.equals("linkedin")) {
+			response.sendRedirect("https://www.linkedin.com");
+		}
+		else if(value.equals("yahoo")) {
+			response.sendRedirect("https://www.yahoo.com");
+		}
+		else if(value.equals("svecw")) {
+			response.sendRedirect("https://www.svecw.edu.in");
+		}
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
