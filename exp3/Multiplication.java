038a0f0dff64ab63386bb33396ea454ff73421f7
abithapendela <71589044+abithapendela@users.noreply.github.com>
2021-02-05 16:14:43 +0530

    Delete exp3 directory

--- exp3/Multiplication.java
+++
@@ -1,9 +0,0 @@
-// Interface file
-import java.rmi.*;
-
-public interface Multiplication extends Remote {
-
-	// declaration of remote methods
-
-	public int multiply(int a, int b) throws RemoteException;
-}
